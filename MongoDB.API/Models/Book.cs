1e9b6447d5b1294b04746d708ba2dae7df4f8b14
andyzf <494126372@qq.com>
2019-06-27 13:25:48 +0800

    MongoDB core

---
+++ MongoDB.API/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDB.API.Models
+{
+
+    //db.createCollection('Books')
+    // db.Books.insertMany([{'Name':'Design Patterns','Price':54.93,'Category':'Computers','Author':'Ralph Johnson'}, {'Name':'Clean Code','Price':43.15,'Category':'Computers','Author':'Robert C. Martin'}])
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; }
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+
+        [BsonElement("Category")]
+        public string Category { get; set; }
+
+        [BsonElement("Author")]
+        public string Author { get; set; }
+    }
+}
---
+++ MongoDB.API/Services/BookService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.API.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDB.API.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IBookstoreDatabaseSettings settings, IConfiguration config)
+        {
+            //var client = new MongoClient(settings.ConnectionString);
+            //var database = client.GetDatabase(settings.DatabaseName);
+            //_books = database.GetCollection<Book>(settings.BooksCollectionName);
+            string str = config.GetConnectionString("MongoDBConnString");
+            var client = new MongoClient(str);
+            var database = client.GetDatabase("BookstoreDb");
+            _books = database.GetCollection<Book>("Books");
+
+        }
+        public List<Book> Get() => _books.Find(e=>true).ToList();
+        public Book Get(string id) =>
+            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+        public void Remove(Book book)
+        {
+            _books.DeleteOne(e => e.Id == book.Id);
+        }
+
+        public void Update(string id, Book book) {
+
+            _books.ReplaceOne(e => e.Id == id, book);
+        }
+        public void Remove(string id)
+        {
+            _books.DeleteOne(e => e.Id == id);
+        }
+       
+    }
+}
